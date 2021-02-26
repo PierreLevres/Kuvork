91b30cc8b14316501658bd7a2e0a0f5abc2f9064
plips-nl <62832923+plips-nl@users.noreply.github.com>
2021-02-26 21:30:05 +0100

    changes stamp generator (both brands)

--- tools/generate.stamps.js
+++ tools/generate.stamps.js
@@ -4,25 +4,39 @@ const { getStamp } = require('./european.tools.js');
 
 const SIZE = 1000;
 //Hyundai:
-//const APP_ID = '99cfff84-f4e2-4be8-a5ed-e5b755eb6581';
+const H_APP_ID = '99cfff84-f4e2-4be8-a5ed-e5b755eb6581';
 //Kia:
-const APP_ID = '693a33fa-c117-43f2-ae3b-61a02d24f417';
+const K_APP_ID = '693a33fa-c117-43f2-ae3b-61a02d24f417';
 
 
-const run = async () => {
+const runH = async () => {
   const array = new Array(SIZE).fill('');
   const baseDate = Date.now();
 
   for (let i = SIZE; --i >= 0;) {
     const date = i % 2 === 0 ? baseDate + (i * 1000) : baseDate - (i * 1000);
-    array[i] = await getStamp(`${APP_ID}:${date}`);
+    array[i] = await getStamp(`${H_APP_ID}:${date}`);
     console.clear();
     console.log(`${SIZE - i}/${SIZE}`)
   }
 
-  writeFileSync(join(resolve('.'), 'src', 'tools', 'european.token.collection.ts'), `export default ${JSON.stringify(array)}`);
+  writeFileSync(join(resolve('.'), 'src', 'tools', 'hyundai.european.token.collection.ts'), `export default ${JSON.stringify(array)}`);
+}
+const runK = async () => {
+  const array = new Array(SIZE).fill('');
+  const baseDate = Date.now();
+
+  for (let i = SIZE; --i >= 0;) {
+    const date = i % 2 === 0 ? baseDate + (i * 1000) : baseDate - (i * 1000);
+    array[i] = await getStamp(`${K_APP_ID}:${date}`);
+    console.clear();
+    console.log(`${SIZE - i}/${SIZE}`)
+  }
+
+  writeFileSync(join(resolve('.'), 'src', 'tools', 'kia.european.token.collection.ts'), `export default ${JSON.stringify(array)}`);
 }
 
-run().catch((err) => console.error(err));
+runH().catch((err) => console.error(err));
+runK().catch((err) => console.error(err));
 
 
