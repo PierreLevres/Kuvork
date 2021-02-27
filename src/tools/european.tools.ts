8b11fe26c0c19f717150307e5d2b0a08b22056c5
plips-nl <62832923+plips-nl@users.noreply.github.com>
2021-02-27 09:44:04 +0100

    changes stamp generator (both brands)

--- src/tools/european.tools.ts
+++ src/tools/european.tools.ts
@@ -1,5 +1,9 @@
-import stamps from './european.token.collection';
+import kiastamps from './kia.european.token.collection';
+import hyundaistamps from './hyundai.european.token.collection';
 
-export const getStamp = (): string => {
-  return stamps[Math.floor(Math.random() * stamps.length)];
+
+export const getStamp = (brand: string): string => {
+
+return (brand == 'H') ? (hyundaistamps[Math.floor(Math.random() * hyundaistamps.length)]) : (kiastamps[Math.floor(Math.random() * kiastamps.length)]);
+
 }
--- tools/generate.stamps.js
+++ tools/generate.stamps.js
@@ -3,40 +3,27 @@ const { writeFileSync } = require('fs');
 const { getStamp } = require('./european.tools.js');
 
 const SIZE = 1000;
-//Hyundai:
 const H_APP_ID = '99cfff84-f4e2-4be8-a5ed-e5b755eb6581';
-//Kia:
 const K_APP_ID = '693a33fa-c117-43f2-ae3b-61a02d24f417';
 
 
-const runH = async () => {
+const run = async (AppId, Brand) => {
   const array = new Array(SIZE).fill('');
   const baseDate = Date.now();
 
   for (let i = SIZE; --i >= 0;) {
     const date = i % 2 === 0 ? baseDate + (i * 1000) : baseDate - (i * 1000);
-    array[i] = await getStamp(`${H_APP_ID}:${date}`);
-    console.clear();
-    console.log(`${SIZE - i}/${SIZE}`)
+    array[i] = await getStamp(AppId+`:${date}`);
+    if (i % 50 == 0)
+    {
+      console.log(`${SIZE - i}/${SIZE}`)
+    }
   }
 
-  writeFileSync(join(resolve('.'), 'src', 'tools', 'hyundai.european.token.collection.ts'), `export default ${JSON.stringify(array)}`);
-}
-const runK = async () => {
-  const array = new Array(SIZE).fill('');
-  const baseDate = Date.now();
-
-  for (let i = SIZE; --i >= 0;) {
-    const date = i % 2 === 0 ? baseDate + (i * 1000) : baseDate - (i * 1000);
-    array[i] = await getStamp(`${K_APP_ID}:${date}`);
-    console.clear();
-    console.log(`${SIZE - i}/${SIZE}`)
-  }
-
-  writeFileSync(join(resolve('.'), 'src', 'tools', 'kia.european.token.collection.ts'), `export default ${JSON.stringify(array)}`);
+  writeFileSync(join(resolve('..'), 'src', 'tools', Brand+'.european.token.collection.ts'), `export default ${JSON.stringify(array)}`);
 }
 
-runH().catch((err) => console.error(err));
-runK().catch((err) => console.error(err));
+run(H_APP_ID,'hyundai').catch((err) => console.error(err));
+run(K_APP_ID,'kia').catch((err) => console.error(err));
 
 
